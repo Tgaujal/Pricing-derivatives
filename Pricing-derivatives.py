04deeb72ff0609de86bdebba51a9e4b0f759221e
Tgaujal <165668599+Tgaujal@users.noreply.github.com>
2024-05-20 13:28:40 +0200

    Update Pricing-derivatives.py

--- Pricing-derivatives.py
+++ Pricing-derivatives.py
@@ -22,12 +22,12 @@ def Binomial_model(S, K, r, T, sigma, option_type, N):
             else:
                 option_tree[j,i]=max(0,K-S_tree[j,i],np.exp(-r*dt)*(p*option_tree[j,i+1]+(1-p)*option_tree[j+1,i+1]))
     return option_tree[0,0]
-S=5
-K=10
+S=100
+K=100
 r=0.06
 sigma=0.3
 T=1
 N=100
-option_type='put'
+option_type='call'
 print(Binomial_model(S, K, r, T, sigma, option_type, N))
 
